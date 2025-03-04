2afc00b98b4809ea83a59937264ff0282e15d291
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2025-03-04 17:41:01 +0100

    Make script idempotent

--- install.sh
+++ install.sh
@@ -12,10 +12,13 @@ cuda_architecture=75
 export CUDACXX=$(which nvcc)
 
 # 0. Setup the environment
-source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh $lcg_version $binary_tag
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh $lcg_version $lcg_platform
 
 # 1. Organize stack folder with lb-stack-setup
-curl https://gitlab.cern.ch/rmatev/lb-stack-setup/raw/master/setup.py | python3 - stack
+if [ ! -d stack ]; then
+	curl https://gitlab.cern.ch/rmatev/lb-stack-setup/raw/master/setup.py | python3 - stack
+fi
+
 cd stack
 python utils/config.py binaryTag $binary_tag
 python utils/config.py forwardEnv '["DISPLAY","SSH_AUTH_SOCK","SSH_AGENT_PID","GITCONDDBPATH","X509_USER_PROXY","CUDACXX"]'
@@ -25,7 +28,11 @@ python utils/config.py dataPackages '["DBASE/AppConfig","DBASE/PRConfig","PARAM/
 
 # 2.1. Install VecCore
 veccore_install="$root/stack/VecCore/$install_dir"
-git clone https://github.com/root-project/veccore.git VecCore
+
+if [ ! -d VecCore ]; then
+	git clone https://github.com/root-project/veccore.git VecCore
+fi
+
 cd VecCore
 git checkout v0.8.2
 cmake -S. -B $build_dir -DCMAKE_INSTALL_PREFIX="$veccore_install"
@@ -34,7 +41,11 @@ cd ..
 
 # 2.2. Install VecGeom
 vecgeom_install="$root/stack/VecGeom/$install_dir"
-git clone ssh://git@gitlab.cern.ch:7999/VecGeom/VecGeom.git
+
+if [ ! -d VecGeom ]; then
+	git clone ssh://git@gitlab.cern.ch:7999/VecGeom/VecGeom.git
+fi
+
 cd VecGeom
 git checkout v2.0.0-rc.3
 cmake -S. -B $build_dir -DCMAKE_INSTALL_PREFIX="$vecgeom_install" \
@@ -54,7 +65,11 @@ make Geant4 BUILDFLAGS="-j6"
 
 # 2.4. Install G4HepEm
 g4hepem_install="$root/stack/G4HepEm/$install_dir"
-git clone https://github.com/mnovak42/g4hepem.git G4HepEm
+
+if [ ! -d G4HepEm ]; then
+	git clone https://github.com/mnovak42/g4hepem.git G4HepEm
+fi
+
 cd G4HepEm
 git checkout 20250220
 
@@ -72,22 +87,29 @@ cd ..
 
 # 2.5. Install AdePT
 adept_install="$root/stack/AdePT/$install_dir"
-git clone https://github.com/apt-sim/AdePT.git
+
+if [ ! -d AdePT ]; then
+	git clone https://github.com/apt-sim/AdePT.git
+fi
+
 cd AdePT
 cmake -S. -B $build_dir -DCMAKE_INSTALL_PREFIX="$adept_install" \
     -DCMAKE_PREFIX_PATH="$veccore_install;$vecgeom_install;$g4hepem_install;$geant4_install" \
     -DCMAKE_CUDA_ARCHITECTURES=$cuda_architecture \
     -DWITH_FLUCT=ON \
     -DBUILD_TESTING=OFF \
-    -DCMAKE_BUILD_TYPE=Debug
+	-DCMAKE_BUILD_TYPE=Release
 cmake --build $build_dir --target install -- -j6
 cd ..
 
 # 3. Install Gaussino
 gaussino_install="$root/stack/Gaussino/$install_dir"
-make Gaussino/checkout
-cd Gaussino
-git checkout jbenavid-adept-integration
-cd ..
+
+if [ ! -d Gaussino ]; then
+	make Gaussino/checkout
+	cd Gaussino
+	git checkout jbenavid-adept-integration
+	cd ..
+fi
 python utils/config.py -- cmakeFlags.Gaussino "-DWITH_ADEPT=ON -DAdePT_DIR=$adept_install/lib64/cmake/AdePT -DG4VG_DIR=$adept_install/lib64/cmake/G4VG"
 make Gaussino BUILDFLAGS='-j6'
