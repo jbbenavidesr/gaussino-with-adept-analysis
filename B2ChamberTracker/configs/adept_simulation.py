cfa54427a6a83022a14f206d180b9ec400288767
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2025-09-01 17:12:58 +0200

    Some results of B2

--- B2ChamberTracker/configs/adept_simulation.py
+++ B2ChamberTracker/configs/adept_simulation.py
@@ -17,8 +17,10 @@ GaussinoSimulation(
 )
 
 GiGaMTRunManagerFAC("GiGaMT.GiGaMTRunManagerFAC").InitCommands = [
+    "/adept/verbose 4",
     "/adept/setCUDAStackLimit 8192",
     "/adept/CallUserTrackingAction true",
+    "/adept/CallUserSteppingAction true",
     "/adept/setTrackInAllRegions true",
     "/adept/setMillionsOfTrackSlots 14",
     "/adept/setMillionsOfHitSlots 24",
